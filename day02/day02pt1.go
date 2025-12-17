dc7fe4b8e0cf9e83ec87db2f67944a5a947831d8
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-17 13:20:08 +0100

    feat: add day02pt1

---
+++ day02/day02pt1.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sum int
+
+func main() {
+	file, err := os.Open("day02pt1Input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		tempInput := strings.Split(scanner.Text(), ",")
+		for _, element := range tempInput {
+			checkSequenze(element)
+		}
+		fmt.Println("sum", sum)
+	}
+}
+
+func checkSequenze(givenRange string) {
+	tempRange := strings.Split(givenRange, "-")
+	start, err := strconv.Atoi(string(tempRange[0]))
+	end, err := strconv.Atoi(string(tempRange[1]))
+	if err != nil {
+		panic(err)
+	}
+
+	for i := start; i <= end; i++ {
+		if start <= 99 {
+			if checkOneDigit(i) {
+				sum += i
+			}
+		} else {
+			if checkManyDigits(i) {
+				sum += i
+			}
+		}
+	}
+}
+
+func checkOneDigit(oneDigit int) bool {
+	s := strconv.Itoa(oneDigit)
+
+	if oneDigit <= 99 {
+		for i := 0; i < len(s)-1; i++ {
+			if s[i] == s[i+1] {
+				fmt.Println("one", s)
+				return true
+			}
+		}
+		return false
+
+	} else {
+		return false
+	}
+}
+
+func checkManyDigits(manyDigit int) bool {
+	s := strconv.Itoa(manyDigit)
+	if len(s)%2 == 0 {
+
+		mid := len(s) / 2
+		sequence := s[:mid]
+		checkHalf := s[mid:]
+
+		if sequence == checkHalf {
+			return true
+		}
+	}
+	return false
+}
